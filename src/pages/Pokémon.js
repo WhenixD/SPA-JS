b218f1939e7c79bce3f046d826b2df79158437b1
WhenixD <whenix.developer@gmail.com>
2021-07-13 10:51:47 -0500

    Create templates

---
+++ src/pages/Pokémon.js
@@ -0,0 +1,20 @@
+const Pokémon = () => {
+  const view = `
+    <div class="pokémon-inner">
+      <article class="pokémon-card">
+        <img src="image" alt="name">
+        <h2>
+          Name
+        </h2>
+      </article>
+      <article class="pokémon-card">
+        <h3>Types:</h3>
+        <h3>Order:</h3>
+        <h3>Weight:</h3>
+      </article>
+    </div>
+  `;
+  return view;
+};
+
+export default Pokémon;
---
+++ src/template/Header.js
@@ -0,0 +1,24 @@
+import Home from "../pages/Home";
+
+const Header = () => {
+  const view = `
+    <div class="header-main">
+      <div class="header-logo">
+        <h1>
+          <a href="/">
+            Pokidex.co
+          </a>
+        </h1>
+      </div>
+      <div class="header-nav">
+        <a href="#/about/">
+          About
+        </a>
+      </div>
+    </div>
+  `;
+
+  return view;
+};
+
+export default Header;
