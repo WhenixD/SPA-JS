bc8329aeb2782882ba3fa9749b8859fe8b77468c
WhenixD <whenix.developer@gmail.com>
2021-07-14 11:36:22 -0500

    Generate each Pokémon page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import getPokémon from "../utils/getPokémon";
 
-const pokémonLimit = 10;
+const API = "https://pokeapi.co/api/v2/pokemon/";
+const pokémonLimit = 50;
 let pokémonList = [];
 
 const Home = async () => {
@@ -10,16 +11,30 @@ const Home = async () => {
   }
   const viewPokémon = `
       <div class="pokémon">
-      ${pokémonList.map(
-        (pokémonList) => `
+      ${pokémonList
+        .map(
+          (pokémonList) => `
         <article class="pokémon-item">
-          <a href="#/${pokémonList.id}/">
-            <img src="${pokémonList.sprites.front_default}" alt="${pokémonList.name}">
+          <a href="/#/${pokémonList.id}">
+            <img src="${pokémonList.sprites.front_default}" alt="${
+            pokémonList.name
+          }">
             <h2>${pokémonList.name}</h2>
+            <div class="pokémon-type">
+            <h3>
+              ${pokémonList.types[0].type.name}
+            </h3>
+              ${
+                pokémonList.types[1]
+                  ? `<h3>${pokémonList.types[1].type.name}</h3>`
+                  : ``
+              }
+              </div>
           </a>
         </article>
         `
-      ).join("")}
+        )
+        .join("")}
       </div>
     `;
   return viewPokémon;
--- src/pages/Pokémon.js
+++ src/pages/Pokémon.js
@@ -1,14 +1,42 @@
-const Pokémon = () => {
+import getHash from "../utils/getHash";
+import getPokémon from "../utils/getPokémon";
+
+const Pokémon = async () => {
+  const id = getHash();
+  const pokémon = await getPokémon(id);
   const view = `
     <div class="pokémon-inner">
       <article class="pokémon-card">
-        <img src="image" alt="name">
-        <h2>Name</h2>
+        <img src="${pokémon.sprites.front_default}" alt="${pokémon.name}">
+        <h2>
+          Name
+        </h2>
+        <span>
+          ${pokémon.name}
+        </span>
       </article>
       <article class="pokémon-card">
-        <h3>Types:</h3>
-        <h3>Number:</h3>
-        <h3>Weight:</h3>
+        <h3>
+          Number: 
+        </h3>
+          <span>
+            ${pokémon.id}
+          </span>
+        <h3>
+          Types:
+        </h3>
+          <span>
+            ${pokémon.types[0].type.name}
+          </span>
+          ${
+            pokémon.types[1] ? `<span>${pokémon.types[1].type.name}</span>` : ``
+          } 
+        <h3>
+          Weight: 
+        </h3>
+            <span>
+              ${pokémon.weight}
+            </span>
       </article>
     </div>
   `;
