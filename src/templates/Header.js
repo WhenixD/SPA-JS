d0050c9bf8eab1d0e9319f2ecc74e1080c0204cf
WhenixD <whenix.developer@gmail.com>
2021-07-15 18:15:33 -0500

    Correct href because idk what I am doing

--- src/templates/Header.js
+++ src/templates/Header.js
@@ -3,7 +3,7 @@ const Header = () => {
     <div class="header-main">
       <div class="header-logo">
         <h1>
-          <a href="/SPA-JS/">
+          <a href="SPA-JS/">
             Pokidex.co
           </a>
         </h1>
