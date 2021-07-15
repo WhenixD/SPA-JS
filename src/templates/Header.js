57e3800309bc9fd5d55a05accecfa4fcf9bea6c1
WhenixD <whenix.developer@gmail.com>
2021-07-15 18:09:15 -0500

    git

--- src/templates/Header.js
+++ src/templates/Header.js
@@ -3,7 +3,7 @@ const Header = () => {
     <div class="header-main">
       <div class="header-logo">
         <h1>
-          <a href="/">
+          <a href="SPA-JS/">
             Pokidex.co
           </a>
         </h1>
